a8b798b10f2b39831ed115f167223d435de7bdb0
manoj240604 <manojmukodihalli@gmail.com>
2026-07-16 10:06:10 +0530

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Demo App version 3.0 ");
+    res.send("Welcome to Demo App version 4.0 ");
 });
 
 app.listen(3000, () => {
